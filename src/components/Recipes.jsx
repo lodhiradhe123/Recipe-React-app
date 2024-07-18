3ced18588805721ffd0a502dff29bb234add3fd9
radhe <radheshyamlodhi48@gmail.com>
2024-07-19 00:40:12 +0530

    (create-react-context)

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import Card from "./Card";
 import { Link, useLocation } from "react-router-dom";
+import { userContext } from "../context/Context";
 
 const Recipes = () => {
+      const [recipy, setRecipy]= useContext(userContext)
+    //   console.log(recipy);
     const { pathname } = useLocation();
     return (
         <div className=" ">
@@ -12,18 +15,14 @@ const Recipes = () => {
                 aperiam?
             </p>
             <div className="recipe-cards mt-[5%]  flex flex-wrap p-5">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                {recipy.length>0?recipy.map((dish,i)=><Card dish={dish} key={i}  />):<h1 className="text-2xl font-bold">loading....</h1>}
             </div>
             {pathname === "/recipes" && (
                 <Link
                     to="/create-recipe"
                     className="cursor-pointer rounded-md absolute top-[15%] py-2 px-5 left-[10%]  bg-green-200 gap-x-3 flex items-center"
                 >
-                    <i class="text-3xl text-green-600 ri-add-box-line"></i>
+                    <i className="text-3xl text-green-600 ri-add-box-line"></i>
                     Create Recipe
                 </Link>
             )}
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import Context from "./context/Context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <Context>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
+  </Context>
 );
